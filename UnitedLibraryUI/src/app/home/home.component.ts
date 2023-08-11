1db7b3702a4a759eda4bcaa6c822d5c65cb8e959
sunfjower <peremena.work@gmail.com>
2023-08-11 19:19:34 +0300

    style: landing page UI updated.
    Added styled search bar with a button.
    Imported fontawesome component for a search-button icon.
    Setted up bacground color for all pages.

--- UnitedLibraryUI/src/app/home/home.component.ts
+++ UnitedLibraryUI/src/app/home/home.component.ts
@@ -1,32 +1,23 @@
 import { style } from "@angular/animations";
 import { Component } from "@angular/core";
-
-interface Novel
-{
-  name: string
-  isFamous: boolean
-}
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
+    standalone: true,
+    imports: [FontAwesomeModule],
     selector: 'home-app',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent {
-    title = 'UnitedLibraryUI';
 
-    novels : Novel[] = [{
-      name: "Fisrt novel",
-      isFamous: false
-    }, {
-      name: "Second novel",
-      isFamous: true
-      }]
-  
-  
+  searchIcon = faMagnifyingGlass;
+
     constructor() {
     }
+
   
     handleClick(value: any) {
       console.log(value);
