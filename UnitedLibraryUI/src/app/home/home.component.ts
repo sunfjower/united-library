44871d9ae13bdb2af48ffdf9779a4d1ffa20b285
sunfjower <peremena.work@gmail.com>
2023-08-23 23:39:38 +0300

    style: updated navbar UI.
    Moved navbar from the shared modules to the landing
    page. Updated navbar desing.
    
    Added 'Roboo' font and defined CSS variables with
    colors.

--- UnitedLibraryUI/src/app/home/home.component.ts
+++ UnitedLibraryUI/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { Router, Routes } from "@angular/router";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { RouterModule } from '@angular/router';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     standalone: true,
-    imports: [FontAwesomeModule],
+    imports: [FontAwesomeModule, RouterModule],
     selector: 'home-app',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
